070f80f42b4fa2378336bb85e9c1e431537010b6
richard1230 <yongjinhu1230@gmail.com>
2020-11-14 02:10:07 +0000

    Built out initial form and partial call for comments along with contro…

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  
+  
+  def create
+      @comments = current_user.comments.build(comment_params)
+  end
+  
+  private
+  
+   def comment_params
+     params.require(:comments).permit(:content)
+   end
+  
 end
