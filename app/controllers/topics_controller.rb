7942344b44eef1ef98c2c91f33eedac5a6592be1
richard1230 <yongjinhu1230@gmail.com>
2020-11-14 09:11:34 +0000

    Implemented the blog widget for topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,7 @@
 class TopicsController < ApplicationController
   layout 'blog'
-
+  before_action :set_sidebar_topics
+  
   def index
     @topics = Topic.all
   end
@@ -14,5 +15,11 @@ class TopicsController < ApplicationController
     else
       @blogs = @topic.blogs.published.recent.page(params[:page]).per(5)
     end
+  end
+  
+  private
+
+  def set_sidebar_topics
+    @side_bar_topics = Topic.with_blogs
   end
 end
